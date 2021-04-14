519c9fb6b9684f2f23e3ee863bf8fbd6ab837b37
asma971 <80236998+asma971@users.noreply.github.com>
2021-04-14 12:03:50 +0400

    Add files via upload

---
+++ PyLib1/index11.py
@@ -0,0 +1,9 @@
+#Program to test over cloud (odd numbers)
+print("Program for printing the aseries of odd numbers")
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+print("This is an odd number.")
+else:
+print("This is an even number.")
+     
